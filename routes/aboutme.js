9241f44df12acb3bb292b802cbadb2746600cf24
Spencer Jackson <spencerja8@gmail.com>
2017-02-13 21:46:57 -0500

    not actual first commit but it is for this repo

---
+++ routes/aboutme.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Spencer on 2/13/2017.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+/* GET the index page and input variables */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'About me page' });
+});
+
+module.exports = router;
